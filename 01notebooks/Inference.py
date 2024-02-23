bbfa07df97148724ee8ed6b23356c4c51c0db0fe
IvanParedesR <ivanresendiz25@gmail.com>
2024-02-22 22:20:48 -0600

    Cambio

--- 01notebooks/Inference.py
+++ 01notebooks/Inference.py
@@ -1,10 +1,10 @@
 import seaborn as sns
-import statsmodels.api as smf #https://www.statsmodels.org/v0.10.2/importpaths.html
-import statsmodels.formula.api as smq
-import statsmodels.regression.linear_model as sm
-from sklearn.model_selection import train_test_split #https://scikit-learn.org/stable/
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+#import statsmodels.api as smf #https://www.statsmodels.org/v0.10.2/importpaths.html
+#import statsmodels.formula.api as smq
+#import statsmodels.regression.linear_model as sm
+#from sklearn.model_selection import train_test_split #https://scikit-learn.org/stable/
+#from sklearn.linear_model import LinearRegression
+#from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt #https://matplotlib.org/2.0.2/users/pyplot_tutorial.html
 
 
@@ -14,14 +14,15 @@ df_training.describe()
 #Información de los tipos de variable, hay bastantes que son objeto.
 df_training.info()
 
-# Describimos la variable dependiente de precio de venta y 
+#Describimos la variable dependiente de precio de venta 
 print(df_training['SalePrice'].describe())
 
 #Graficamos su histograma
 plt.figure(figsize=(9, 5)) # Establecemos tamaño
-sns.distplot(df_training['SalePrice'], color='r', bins=50, hist_kws={'alpha': 0.4});
+sns.distplot(df_training['SalePrice'], color='r', bins=50, hist_kws={'alpha': 0.4})
 
-# Eliminamos los datos que no son numericos y creamos un mapa de calor con la relación entre las distintas variables.
+#Eliminamos los datos que no son numericos y creamos un mapa de calor 
+#con la relación entre las distintas variables.
 df_corr = df_training.astype("float64",errors='ignore')
 df_corr = df_corr.select_dtypes(exclude="object")
 plt.subplots(figsize=(12,9))
